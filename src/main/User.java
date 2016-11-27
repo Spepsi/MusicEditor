ce9e1467fa346495d7a7b0801f32168ffa69136d
Spepsi <floriangardin@hotmail.fr>
2016-11-27 16:14:38 +0100

    add sharps

--- src/main/User.java
+++ src/main/User.java
@@ -18,15 +18,24 @@ public class User {
 		// Insert a bar
 		Bar b1 = new Bar();
 		// Insert a note in the bar
-		Note n1 = new Note(32,false,0,3,50);
-		Note n2 = new Note(16,false,2,3,50);
-		Note n3 = new Note(8,false,3,3,50);
-		Note n4 = new Note(8,false,4,3,50);
+		Note n1 = new Note(4,false,0,3,50);
+		Note n2 = new Note(4,false,2,3,50);
+		Note n3 = new Note(4,false,4,3,50);
+		Note n4 = new Note(4,false,6,3,50);
+		Note n5 = new Note(4,false,0,4,50);
+		Note n6 = new Note(4,false,2,4,50);
+		Note n7 = new Note(4,false,8,4,50);
+		Note n8 = new Note(4,false,5,4,50);
+
 
 		b1.addNote(n1);
 		b1.addAfter(n1,n2);
 		b1.addAfter(n2,n3);
-		b1.addWith(n3,n4);
+		b1.addAfter(n3,n4);
+		b1.addAfter(n4,n5);
+		b1.addAfter(n5,n6);
+		b1.addAfter(n6,n7);
+		b1.addAfter(n7,n8);
 		s.addBar(b1);
 	}
 
